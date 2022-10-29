cef188325b9c7bb5e04664995d9186f3ebbfc959
ryanmerced <ryan_merced@hotmail.com>
2022-10-29 10:53:53 -0700

    adding more code

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const team = require("./src/team");
+const team = require("./src/teamHTML");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+const teamRoster  = [];
+
+function managerQuestions() {
+    inquirer
+     .prompt([
+        {
+            type: "input",
+            name: "name", 
+            message: "What is the manager's name?",
+        },
+        {
+            type: "input",
+            name: "id", 
+            message: "What is the manager's ID?",
+        },
+        {
+            type: "input",
+            name: "email", 
+            message: "What is the manager's email?",
+        },
+        {
+            type: "input",
+            name: "location", 
+            message: "What is the manager's office location?",
+        },
+     ]).then((answers) => {
+        console.log(answers);
+        const { name, id, email, location } = answers;
+        const manager = new Manager(name, id, email, location);
+
+        const addManager = {
+            title: manager.getTitle(),
+            name: manager.getName(),
+            id: manager.getId(),
+            email: manager.getEmail(),
+            extra: manager.getLocation(),
+        };
+        teamRoster.push(addManager);
+     });
+     
+}
